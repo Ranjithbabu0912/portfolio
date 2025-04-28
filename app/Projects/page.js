7f6b63bbdb07857857941b007e3ebfca14f61ad3
Ranjith Babu <ranjithbabu0912@gmail.com>
2025-04-28 19:32:29 +0530

    new commit

--- app/Projects/page.js
+++ app/Projects/page.js
@@ -1,12 +1,13 @@
 "use client";
-import { assets, workData } from "@/assets/assets";
-import Project from "./components/Project";
-import { useEffect, useState } from "react";
-import Modal from "./components/Modal";
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import Project from "./components/Project";
+import Modal from "./components/Modal";
+import { assets, workData } from "@/assets/assets";
 
-export default function Projects() {
+export default function Home() {
+
     const [isDarkMode, setIsDarkMode] = useState(true);
 
     useEffect(() => {
@@ -35,11 +36,20 @@ export default function Projects() {
 
     return (
         <div>
-            <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/> <br /><br />
+            <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} /> <br />
+            <br />
             <div className="flex items-center justify-around sm:p-10 p-5 scroll-m-0">
                 <h1 className="text-3xl sm:text-6xl">My Projects</h1>
-                <a target="_blank" href="https://github.com/Ranjithbabu0912" className="flex items-center gap-2 text-sm sm:text-xl">
-                    <Image src={isDarkMode ? assets.github_white : assets.github} alt="" className="w-4 sm:w-8" />
+                <a
+                    target="_blank"
+                    href="https://github.com/Ranjithbabu0912"
+                    className="flex items-center gap-2 text-sm sm:text-xl"
+                >
+                    <Image
+                        src={isDarkMode ? assets.github_white : assets.github}
+                        alt=""
+                        className="w-4 sm:w-8"
+                    />
                     Ranjithbabu0912
                 </a>
             </div>
@@ -55,7 +65,7 @@ export default function Projects() {
                         />
                     );
                 })}
-                <Modal modal={modal} projects={workData}/>
+                <Modal modal={modal} projects={workData} />
             </div>
         </div>
     );
